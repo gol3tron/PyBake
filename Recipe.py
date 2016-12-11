7a00fad99bc9f06db42b4a2e25a5d83d384a831b
gol3tron <adamgoler@gmail.com>
2016-12-10 21:57:08 -0800

    updated test template and basic Recipe skeleton

--- Recipe.py
+++ Recipe.py
@@ -1,2 +1,26 @@
-def Recipe():
+def Recipe(type,flavors,restrictions):
     
+    ingredients = []
+    steps = []
+    
+    if (type=="pie")
+
+    else if (type=="cake")
+
+    else if (type=="cookies")
+
+    else
+        print("Bro, do you even PyBake?")
+
+
+def printIngredients():
+
+    ingredients = []
+
+    print(ingredients)
+
+def printSteps():
+
+    steps = []
+
+    print(steps)
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-recipe = 
+restrictions = ["vegan","gluten free","dairy free","egg free"]
+
+flavors = ["strawberry","chocolate"]
+
+recipe = Recipe("pie".flavors,restrictions)
+
+recipe.printIngredients()
+recipe.printSteps()
